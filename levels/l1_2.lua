b241dbceb65d627b702952064b99ac3b707fe9a4
Flare <flare.the.protogen.yt@gmail.com>
2025-04-29 07:50:39 -0600

    adj

--- levels/l1_2.lua
+++ levels/l1_2.lua
@@ -3,6 +3,6 @@ return function(level)
     level:Platform(level:Point2(470,100),level:Point2(100,20))
     level:Platform(level:Point2(100,600),psz)
     level:Platform(level:Point2(200,400),psz)
-    level:exit(level:Point2(500,300))
+    level:exit(level:Point2(470,300))
     level:player(level:Point2(100, 400))
 end
